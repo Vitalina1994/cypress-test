7dd0cd115b80e9b2c439b40bdace768406c4fd84
vitalina <vitalina.zubko@mev.com>
2023-10-13 01:58:09 +0300

    added homeTasks for cypress-locators topic

---
+++ cypress/e2e/stepper_app/stepper_home_task2.spec.js
@@ -0,0 +1,22 @@
+describe('Stepper page', ()=>{
+  beforeEach(()=>{
+    cy.visit('/pages/layout/stepper')
+  })
+
+  it('Vertical stepper should display correct title on each step', () => {
+    cy.get('nb-stepper[orientation="vertical"] h3').as('stepperTitle')
+    cy.get('nb-stepper[orientation="vertical"] button').last().as('nextButton')
+
+    cy.get('@stepperTitle').should('have.text', 'Step content #1')
+    cy.get('@nextButton').click()
+
+    cy.get('@stepperTitle').should('have.text', 'Step content #2')
+    cy.get('@nextButton').click()
+
+    cy.get('@stepperTitle').should('have.text', 'Step content #3')
+    cy.get('@nextButton').click()
+
+    cy.get('@stepperTitle').should('have.text', 'Step content #4')
+    cy.get('@nextButton').should('be.disabled')
+  })
+})
